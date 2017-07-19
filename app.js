8a2f7b0146f3212256431e3452a870388fe2c22a
Edwin Moses <workockmoses@gmail.com>
2017-07-19 16:43:08 +0300

    middleware  concept

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ var path = require('path');
 
 var app = express();
 
+/*
+var logger = (req, res, next) => {
+  console.log("Logging...");
+  next();
+}
+*/
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+})
+
 app.listen(3000, () => {
   console.log("Server listening on....");
 });
